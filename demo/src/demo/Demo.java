91d66c4a0eca4fe3d2f3ae6f0a1ca96b97c2560e
tohieungoi <tohieungoi@gmail.com>
2020-04-10 14:43:49 +0700

    v1.1

---
+++ demo/src/demo/Demo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+/**
+ *
+ * @author tohie
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Helo");
+        
+        // TODO code application logic here
+        
+    }
+    
+}
